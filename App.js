b4c2f0d4bd2b534f409591153ef91388d794dd8f
thawatchai chintrakul <thawatchaichintrakun@gmail.com>
2025-07-27 16:41:14 +0700

    feat: set up React Navigation with Home Details and Profile screens

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./components/Navigations";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Thawatchai Chintrakul DBT102</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFFFFF",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
